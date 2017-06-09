8feb852e0ad89f8c73f6d82fb0d86fa8ba68dcc2
Erhun Akbas <erhun@perronwei.nl>
2017-06-09 14:48:49 +0200

    working on login

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -28,6 +28,9 @@
 
 </head>
 <body class="<?php echo View::getBodyClass($filename); ?>">
+  <?php
+    if (($_SERVER['REQUEST_URI'] == '/uFlyProject/login/index') OR ($_SERVER['REQUEST_URI'] == '/uFlyProject/register/index')) {
+        } else { ?>
   <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
     <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
       <div class="mdl-layout__header-row">
@@ -60,32 +63,25 @@
           <img class="demo-avatar" src='<?= Session::get('user_avatar_file'); ?>' />
           <?php } ?>
         <div class="demo-avatar-dropdown">
-          <span>hello@example.com</span>
+          <span><?= Session::get('user_name') ?></span>
           <div class="mdl-layout-spacer"></div>
           <button id="accbtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
             <i class="material-icons" role="presentation">arrow_drop_down</i>
             <span class="visuallyhidden">Accounts</span>
           </button>
           <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="accbtn">
-            <li class="mdl-menu__item">hello@example.com</li>
-            <li class="mdl-menu__item">info@example.com</li>
-            <li class="mdl-menu__item"><i class="material-icons">add</i>Add another account...</li>
+            <li class="mdl-menu__item"><?= Session::get('user_email') ?></li>
           </ul>
         </div>
       </header>
       <nav class="demo-navigation mdl-navigation ">
-        <a class="mdl-navigation__link" <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?>  class="mdl-navigation__link" href="<?php echo Config::get('URL'); ?>index/index"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Index</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">inbox</i>Inbox</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">delete</i>Trash</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">report</i>Spam</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">forum</i>Forums</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">flag</i>Updates</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">local_offer</i>Promos</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">shopping_cart</i>Purchases</a>
-        <a class="mdl-navigation__link" href=""><i class="material-icons" role="presentation">people</i>Social</a>
+        <a class="mdl-navigation__link <?php if (View::checkForActiveController($filename, "index")) { echo ' active" '; } ?>" href="<?= Config::get('URL') ?>index/index"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+        <a class="mdl-navigation__link <?php if (View::checkForActiveController($filename, "profile")) { echo ' active" '; } ?>" href="<?= Config::get('URL') ?>profile/index"><i class="material-icons" role="presentation">inbox</i>Profiel</a>
+        <a class="mdl-navigation__link <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' active" '; } ?>" href="<?= Config::get('URL') ?>dashboard/index"><i class="material-icons" role="presentation">delete</i>Dashboard</a>
         <div class="mdl-layout-spacer"></div>
-        <!-- <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a> -->
+        <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a>
       </nav>
     </div>
     <main class="mdl-layout__content mdl-color--grey-100">
       <div class="mdl-grid demo-content">
+      <?php } ?>
