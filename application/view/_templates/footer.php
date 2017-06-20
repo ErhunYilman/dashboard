26eb4e0a517e764b3c301145f5a0d2d989167fdb
Erhun Akbas <erhun@perronwei.nl>
2017-06-20 13:24:57 +0200

    deleted footter links from dashboard branch

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,67 +1,3 @@
-<ul class="navigation">
-    <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
-        <a href="<?php echo Config::get('URL'); ?>index/index">Index</a>
-    </li>
-    <li <?php if (View::checkForActiveController($filename, "profile")) { echo ' class="active" '; } ?> >
-        <a href="<?php echo Config::get('URL'); ?>profile/index">Profiles</a>
-    </li>
-    <?php if (Session::userIsLoggedIn()) { ?>
-        <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
-            <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
-        </li>
-        <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
-            <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
-        </li>
-    <?php } else { ?>
-        <!-- for not logged in users -->
-        <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
-            <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
-        </li>
-        <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
-            <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-        </li>
-    <?php } ?>
-</ul>
-
-<!-- my account -->
-<ul class="navigation right">
-<?php if (Session::userIsLoggedIn()) : ?>
-    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-        <a href="<?php echo Config::get('URL'); ?>user/index">My Account</a>
-        <ul class="navigation-submenu">
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/changeUserRole">Change account type</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/editAvatar">Edit your avatar</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/editusername">Edit my username</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/edituseremail">Edit my email</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/changePassword">Change Password</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>login/logout">Logout</a>
-            </li>
-        </ul>
-    </li>
-    <?php if (Session::get("user_account_type") == 7) : ?>
-        <li <?php if (View::checkForActiveController($filename, "admin")) {
-            echo ' class="active" ';
-        } ?> >
-            <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
-        </li>
-    <?php endif; ?>
-<?php endif; ?>
-</ul>
-<?php
-$now = new DateTime();
-$date = $now->getTimestamp();
- ?>
 <div class="footer"></div>
   <!-- close class="wrapper" -->
 
@@ -75,7 +11,7 @@ $date = $now->getTimestamp();
     <!-- jQuery UI 1.10.3 -->
     <script src="<?php echo Config::get('URL'); ?>js/jquery-ui-1.12.1.min.js" type="text/javascript"></script>
     <!-- script -->
-    <script src="<?php echo Config::get('URL'); ?>js/script.js?_=<?=md5($date)?>" type="text/javascript"></script>
+    <script src="<?php echo Config::get('URL'); ?>js/script.js" type="text/javascript"></script>
 
     <script src="<?php echo Config::get('URL'); ?>js/material.min.js" type="text/javascript"></script>
   </body>
