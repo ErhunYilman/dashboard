775159f2dc987454c63ee5d4b3d7b97007f24734
Erhun Akbas <erhun@perronwei.nl>
2017-10-03 22:18:56 +0200

    Zoekveld verwijderd

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -74,7 +74,7 @@
 							</li>
 						</ul>
 
-						<form class="navbar-form navbar-right" role="search">
+						<!-- <form class="navbar-form navbar-right" role="search">
 							<div class="form-group  is-empty">
 								<input type="text" class="form-control" placeholder="Search">
 								<span class="material-input"></span>
@@ -82,7 +82,7 @@
 							<button type="submit" class="btn btn-white btn-round btn-just-icon">
 								<i class="material-icons">search</i><div class="ripple-container"></div>
 							</button>
-						</form>
+						</form> -->
 					</div>
 				</div>
 			</nav>
